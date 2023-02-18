2da63cc072a87c51f103792fbed448faffd527fb
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-02-18 18:57:58 +0200

    Add wizard movement

--- src/js/main.js
+++ src/js/main.js
@@ -8,29 +8,54 @@ document.addEventListener('keydown', onKeyDown);
 document.addEventListener('keyup', onKeyUp);
 
 let keys = {};
+let player = {
+	x: 150,
+	y: 100,
+};
+let game = {
+	speed: 5,
+};
 
 function onKeyDown(e) {
 	keys[e.code] = true;
 	console.log(keys);
 }
+
 function onKeyUp(e) {
 	keys[e.code] = false;
 	console.log(keys);
 }
 
 function gameAction() {
-	console.log('loop');
+	const wizard = document.querySelector('.wizard');
+
+	if (keys.ArrowUp || keys.KeyW) {
+		player.y -= game.speed;
+	}
+	if (keys.ArrowDown || keys.KeyS) {
+		player.y += game.speed;
+	}
+	if (keys.ArrowLeft || keys.KeyA) {
+		player.x -= game.speed;
+	}
+	if (keys.ArrowRight || keys.KeyD) {
+		player.x += game.speed;
+	}
+
+	wizard.style.top = player.y + 'px';
+	wizard.style.left = player.x + 'px';
+
 	window.requestAnimationFrame(gameAction);
 }
 
 function onGameStart() {
-    gameStart.classList.add('hide');
-    
+	gameStart.classList.add('hide');
+
 	const wizard = document.createElement('div');
 	wizard.classList.add('wizard');
-	wizard.style.top = '200px';
-	wizard.style.left = '200px';
-    gameArea.appendChild(wizard);
-    
+	wizard.style.top = player.y + 'px';
+	wizard.style.left = player.x + 'px';
+	gameArea.appendChild(wizard);
+
 	window.requestAnimationFrame(gameAction);
 }
